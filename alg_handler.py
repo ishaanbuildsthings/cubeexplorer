1440a0291cd3798520b46ec8ff57f9464c928eb9
tGoh98 <tmg5@rice.edu>
2022-11-27 23:54:19 -0800

    connect solver

--- alg_handler.py
+++ alg_handler.py
@@ -86,7 +86,7 @@ def clean_up_intersection(lista, listb):
     elif not list2:
         return list1
     elif list1[-1][0] != list2[0][0]:
-        print(f"list1: {list1} list2: {list2}")
+        # print(f"list1: {list1} list2: {list2}")
         return list1 + list2
     else:
         if list1[-1][-1] == "'":
